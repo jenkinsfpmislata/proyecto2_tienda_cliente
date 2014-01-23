253e78588f4e254f3e4232dd4201ef5050993d5f
alumno <alumno@2A5PC22>
2014-01-23 13:08:48 +0100

    adminClient sin dos inserts

--- adminClient.php
+++ adminClient.php
@@ -40,127 +40,127 @@
                     $('#tabla').html(datos);
                 }
             });
-
-
-
-
-            function refrescar() {
-
-                $.ajax({
-                    dataType: 'json',
-                    url: 'phps/admin/clienteAdmin.php',
-                    success: function(data) {
-                        datos = '<tr id="primerTr"><td>#</td><td>Name</td><td>Nick</td><td>@</td><td>Imagen<td>Password</td><td>Role</td><td>Edit</td></tr>';
-                        $.each(data, function(index) {
-                            datos += '<tr>';
-                            datos += '<td>' + data[index].idCliente + '<td>' + data[index].nombreCliente + '<td>' + data[index].nick + '<td>' + data[index].email + '<td>' + data[index].imagen + '<td>' + data[index].contrasenya + '<td>' + data[index].rol + '<td><div class="btn btn-default btn-sm" onclick="prepareUpdate(' + data[index].idCliente + ')" data-toggle="modal" data-target="#myModalUpdate"><span class="glyphicon glyphicon-edit"></span></div><a class="btn btn-default btn-sm" href="javascript:deleteCliente(' + data[index].idCliente + ')"><span class="glyphicon glyphicon-remove"></span></a></td>';
-                            datos += '</tr>';
-                        });
-                        $('#tabla').html(datos);
-                    }
-                });
-
+			
+        });
+		
+		
+		
+		
+		function refrescar(){
+		
+		$.ajax({
+                dataType: 'json',
+                url: 'phps/admin/clienteAdmin.php',
+			
+                success: function(data) {
+				   datos = '<tr id="primerTr"><td>#</td><td>Name</td><td>Nick</td><td>@</td><td>Imagen<td>Password</td><td>Role</td><td>Edit</td></tr>';
+                   $.each(data, function(index) {
+				  datos += '<tr>';
+					datos += '<td>'+ data[index].idCliente + '<td>'+ data[index].nombreCliente +'<td>'+data[index].nick+'<td>'+ data[index].email +'<td>'+data[index].imagen+'<td>'+data[index].contrasenya+'<td>'+data[index].rol+'<td><div class="btn btn-default btn-sm" onclick="prepareUpdate('+data[index].idCliente+')" data-toggle="modal" data-target="#myModalUpdate"><span class="glyphicon glyphicon-edit"></span></div><a class="btn btn-default btn-sm" href="javascript:deleteCliente('+ data[index].idCliente +')"><span class="glyphicon glyphicon-remove"></span></a></td>';
+                    datos += '</tr>';
+				   });
+                    $('#tabla').html(datos);
+                }
+            });
+			
+        };
+		
+		
+		function deleteCliente(id){
+				$.ajax({
+					type:'GET',
+					url: 'phps/admin/deleteIdCliente.php?id='+id,
+					success: function() {
+				   alert("Registro "+id+" Borrado");
+				   refrescar();
+                }
+					
+					
+				});
+		};
+		
+		function insertar(){
+			 insertar=$('#insertarCliente').serialize();
+			 
+		$.ajax({
+				dataType: 'json',
+                url: 'phps/admin/insertCliente.php',
+				type: 'POST',
+				data:insertar,
+                success: function() {
+			
+                  alert("Cliente Insertado");
+				  refrescar();
+				  
             }
-            ;
+        });
 
 
-            function deleteCliente(id) {
-                $.ajax({
-                    type: 'GET',
-                    url: 'phps/admin/deleteIdCliente.php?id=' + id,
-                    success: function() {
-                        alert("Registro " + id + " Borrado");
-                        refrescar();
-                    }
+};
+function prepareUpdate(id){
+
+		$.ajax({
+					dataType: 'json',
+					url: 'phps/admin/selectClient.php?id='+id,
+					type:'GET',
+					
+					success: function(data) {
+				
+					index=0;
+					datos='<form id="updatar" class="form-horizontal"  ><input id="oculto" class="form-control" name="id" value="'+ data[index].idCliente + '"></input>Name<input class="form-control" name="nombre" value="'+ data[index].nombreCliente + '"  ></input>Nick<input class="form-control" name="nick" value="'+ data[index].nick + '"></input>E-mail<input class="form-control" name="email" value="'+ data[index].email + '"></input>Password<input class="form-control"  type="password" name="pass" value="'+ data[index].contrasenya + '"></input>Client kind<select class="form-control"><option>Admin</option><option>User</option></select></div><a class="btn btn-success" href="javascript:update()">Update</a>   <button type="button" class="btn btn-default" data-dismiss="modal">Close</button></form>'
+				   $('#updateDiv').html(datos);
+				   	
+                }
+					
+					
+				});
 
 
-                });
-            }
-            ;
+};
 
-            function insertar() {
-                insertar = $('#insertarCliente').serialize();
+function update(){
+		
+		 updatar=$('#updatar').serialize();
 
-                $.ajax({
-                    dataType: 'json',
-                    url: 'phps/admin/insertCliente.php',
-                    type: 'POST',
-                    data: insertar,
-                    success: function(data) {
+$.ajax({
+					url: 'phps/admin/updateClient.php',
+					type: 'POST',
+					data:updatar,
+					success: function() {
+					 alert("Registro "+id+" updated");
+				   refrescar();
 
-                        alert("Cliente Insertado");
-                        refrescar();
+						}
+				});
+				
+};
+function buscar(){
+busc=$('#searchForm').serialize();
+alert(busc);
+ 
+ $.ajax({
+					dataType: 'json',
+					url: 'phps/admin/buscarCliente.php',
+					type: 'POST',
+					data:busc,
+					success: function(data) {
+					datos = '<tr id="primerTr"> <td>#</td><td>Name</td><td>Nick</td><td>@</td><td>Image<td>Password</td><td>Role</td><td>Edit</td></tr>';
+					
+                   $.each(data, function(index) {
+				  
+				  datos += '<tr>';
+					datos += '<td>'+ data[index].idCliente + '<td>'+ data[index].nombreCliente +'<td>'+data[index].nick+'<td>'+ data[index].email +'<td>'+data[index].imagen+'<td>'+data[index].contrasenya+'<td>'+data[index].rol+'<td><div class="btn btn-default btn-sm" onclick="prepareUpdate('+data[index].idCliente+')" data-toggle="modal" data-target="#myModalUpdate"><span class="glyphicon glyphicon-edit"></span></div><a class="btn btn-default btn-sm" href="javascript:deleteCliente('+ data[index].idCliente +')"><span class="glyphicon glyphicon-remove"></span></a></td>';
+                    datos += '</tr>';
+				   });
+                    $('#tabla').html(datos);
+				   }	
+				});
+			
 
-                    }
-                });
+};
+//alert("succes");
 
-
-            }
-            ;
-            function prepareUpdate(id) {
-
-                $.ajax({
-                    dataType: 'json',
-                    url: 'phps/admin/selectClient.php?id=' + id,
-                    type: 'GET',
-                    success: function(data) {
-
-                        index = 0;
-                        datos = '<form id="updatar" class="form-horizontal"  ><input id="oculto" class="form-control" name="id" value="' + data[index].idCliente + '"></input>Name<input class="form-control" name="nombre" value="' + data[index].nombreCliente + '"  ></input>Nick<input class="form-control" name="nick" value="' + data[index].nick + '"></input>E-mail<input class="form-control" name="email" value="' + data[index].email + '"></input>Password<input class="form-control"  type="password" name="pass" value="' + data[index].contrasenya + '"></input>Client kind<select class="form-control"><option>Admin</option><option>User</option></select></div><a class="btn btn-success" href="javascript:update()">Update</a>   <button type="button" class="btn btn-default" data-dismiss="modal">Close</button></form>'
-                        $('#updateDiv').html(datos);
-
-                    }
-
-
-                });
-
-
-            }
-            ;
-
-            function update() {
-
-                updatar = $('#updatar').serialize();
-
-                $.ajax({
-                    url: 'phps/admin/updateClient.php',
-                    type: 'POST',
-                    data: updatar,
-                    success: function() {
-                        alert("Registro " + id + " updated");
-                        refrescar();
-
-                    }
-                });
-
-            }
-            ;
-            function buscar() {
-                busc = $('#searchForm').serialize();
-                alert(busc);
-
-                $.ajax({
-                    dataType: 'json',
-                    url: 'phps/admin/buscarCliente.php',
-                    type: 'POST',
-                    data: busc,
-                    success: function(data) {
-                        datos = '<tr id="primerTr"> <td>#</td><td>Name</td><td>Nick</td><td>@</td><td>Image<td>Password</td><td>Role</td><td>Edit</td></tr>';
-
-                        $.each(data, function(index) {
-
-                            datos += '<tr>';
-                            datos += '<td>' + data[index].idCliente + '<td>' + data[index].nombreCliente + '<td>' + data[index].nick + '<td>' + data[index].email + '<td>' + data[index].imagen + '<td>' + data[index].contrasenya + '<td>' + data[index].rol + '<td><div class="btn btn-default btn-sm" onclick="prepareUpdate(' + data[index].idCliente + ')" data-toggle="modal" data-target="#myModalUpdate"><span class="glyphicon glyphicon-edit"></span></div><a class="btn btn-default btn-sm" href="javascript:deleteCliente(' + data[index].idCliente + ')"><span class="glyphicon glyphicon-remove"></span></a></td>';
-                            datos += '</tr>';
-                        });
-                        $('#tabla').html(datos);
-                    }
-                });
-
-
-            };
-
-       </script>
+        </script>
     </head>
     <body>
         <div id="topbar">
