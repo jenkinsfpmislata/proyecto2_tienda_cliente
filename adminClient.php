7f5a0676092ddccac1aab9b4afe9a982114de829
alumno <alumno@2A5PC22>
2014-01-23 13:00:09 +0100

    adminClient sin dos inserts

--- adminClient.php
+++ adminClient.php
@@ -31,10 +31,10 @@
                 url: 'phps/admin/clienteAdmin.php',
 			
                 success: function(data) {
-				   datos = '<tr id="primerTr"><td>#</td><td>Name</td><td>Nick</td><td>@</td><td>Imagen<td>Password</td><td>Role</td><td>Edit</td></tr>';
+				   datos = '<tr id="primerTr"><td>#</td><td>Name</td><td>Nick</td><td>@</td><td>Image<td>Password</td><td>Role</td><td>Edit</td></tr>';
                    $.each(data, function(index) {
 				  datos += '<tr>';
-					datos += '<td>'+ data[index].idCliente + '<td>'+ data[index].nombreCliente +'<td>'+data[index].nick+'<td>'+ data[index].email +'<td>'+data[index].imagen+'<td>'+data[index].contrasenya+'<td>'+data[index].rol+'<td><a class="btn btn-default btn-sm" data-toggle="modal" data-target="#myModal" href="javascript:prepareUpdate('+ data[index].idCliente +')"><span class="glyphicon glyphicon-edit"></span></a><a class="btn btn-default btn-sm" href="javascript:deleteCliente('+ data[index].idCliente +')"><span class="glyphicon glyphicon-remove"></span></a></td>';
+					datos += '<td>'+ data[index].idCliente + '<td>'+ data[index].nombreCliente +'<td>'+data[index].nick+'<td>'+ data[index].email +'<td>'+data[index].imagen+'<td>'+data[index].contrasenya+'<td>'+data[index].rol+'<td><div class="btn btn-default btn-sm" onclick="prepareUpdate('+data[index].idCliente+')" data-toggle="modal" data-target="#myModalUpdate"><span class="glyphicon glyphicon-edit"></span></div><a class="btn btn-default btn-sm" href="javascript:deleteCliente('+ data[index].idCliente +')"><span class="glyphicon glyphicon-remove"></span></a></td>';
                     datos += '</tr>';
 				   });
                     $('#tabla').html(datos);
@@ -56,7 +56,7 @@
 				   datos = '<tr id="primerTr"><td>#</td><td>Name</td><td>Nick</td><td>@</td><td>Imagen<td>Password</td><td>Role</td><td>Edit</td></tr>';
                    $.each(data, function(index) {
 				  datos += '<tr>';
-					datos += '<td>'+ data[index].idCliente + '<td>'+ data[index].nombreCliente +'<td>'+data[index].nick+'<td>'+ data[index].email +'<td>'+data[index].imagen+'<td>'+data[index].contrasenya+'<td>'+data[index].rol+'<td><a class="btn btn-default btn-sm" data-toggle="modal" data-target="#myModal" href="javascript:prepareUpdate('+ data[index].idCliente +')"><span class="glyphicon glyphicon-edit"></span></a><a class="btn btn-default btn-sm" href="javascript:deleteCliente('+ data[index].idCliente +')"><span class="glyphicon glyphicon-remove"></span></a></td>';
+					datos += '<td>'+ data[index].idCliente + '<td>'+ data[index].nombreCliente +'<td>'+data[index].nick+'<td>'+ data[index].email +'<td>'+data[index].imagen+'<td>'+data[index].contrasenya+'<td>'+data[index].rol+'<td><div class="btn btn-default btn-sm" onclick="prepareUpdate('+data[index].idCliente+')" data-toggle="modal" data-target="#myModalUpdate"><span class="glyphicon glyphicon-edit"></span></div><a class="btn btn-default btn-sm" href="javascript:deleteCliente('+ data[index].idCliente +')"><span class="glyphicon glyphicon-remove"></span></a></td>';
                     datos += '</tr>';
 				   });
                     $('#tabla').html(datos);
@@ -84,10 +84,11 @@
 			 
 		$.ajax({
 				dataType: 'json',
-                url: 'phps/admin/insert.php',
+                url: 'phps/admin/insertCliente.php',
 				type: 'POST',
 				data:insertar,
-                success: function(data) {
+                success: function() {
+			
                   alert("Cliente Insertado");
 				  refrescar();
 				  
@@ -97,13 +98,18 @@
 
 };
 function prepareUpdate(id){
+
 		$.ajax({
+					dataType: 'json',
+					url: 'phps/admin/selectClient.php?id='+id,
 					type:'GET',
-					url: 'phps/admin/updateCliente.php?id='+id,
-					success: function() {
-				   
-				  
-				     $('#updateDiv').html(datos);
+					
+					success: function(data) {
+				
+					index=0;
+					datos='<form id="updatar" class="form-horizontal"  ><input id="oculto" class="form-control" name="id" value="'+ data[index].idCliente + '"></input>Name<input class="form-control" name="nombre" value="'+ data[index].nombreCliente + '"  ></input>Nick<input class="form-control" name="nick" value="'+ data[index].nick + '"></input>E-mail<input class="form-control" name="email" value="'+ data[index].email + '"></input>Password<input class="form-control"  type="password" name="pass" value="'+ data[index].contrasenya + '"></input>Client kind<select class="form-control"><option>Admin</option><option>User</option></select></div><a class="btn btn-success" href="javascript:update()">Update</a>   <button type="button" class="btn btn-default" data-dismiss="modal">Close</button></form>'
+				   $('#updateDiv').html(datos);
+				   	
                 }
 					
 					
@@ -111,6 +117,48 @@ function prepareUpdate(id){
 
 
 };
+
+function update(){
+		
+		 updatar=$('#updatar').serialize();
+
+$.ajax({
+					url: 'phps/admin/updateClient.php',
+					type: 'POST',
+					data:updatar,
+					success: function() {
+					 alert("Registro "+id+" updated");
+				   refrescar();
+
+						}
+				});
+				
+};
+function buscar(){
+busc=$('#searchForm').serialize();
+alert(busc);
+ 
+ $.ajax({
+					dataType: 'json',
+					url: 'phps/admin/buscarCliente.php',
+					type: 'POST',
+					data:busc,
+					success: function(data) {
+					datos = '<tr id="primerTr"> <td>#</td><td>Name</td><td>Nick</td><td>@</td><td>Image<td>Password</td><td>Role</td><td>Edit</td></tr>';
+					
+                   $.each(data, function(index) {
+				  
+				  datos += '<tr>';
+					datos += '<td>'+ data[index].idCliente + '<td>'+ data[index].nombreCliente +'<td>'+data[index].nick+'<td>'+ data[index].email +'<td>'+data[index].imagen+'<td>'+data[index].contrasenya+'<td>'+data[index].rol+'<td><div class="btn btn-default btn-sm" onclick="prepareUpdate('+data[index].idCliente+')" data-toggle="modal" data-target="#myModalUpdate"><span class="glyphicon glyphicon-edit"></span></div><a class="btn btn-default btn-sm" href="javascript:deleteCliente('+ data[index].idCliente +')"><span class="glyphicon glyphicon-remove"></span></a></td>';
+                    datos += '</tr>';
+				   });
+                    $('#tabla').html(datos);
+				   }	
+				});
+			
+
+};
+//alert("succes");
 
         </script>
     </head>
@@ -142,25 +190,33 @@ function prepareUpdate(id){
                 <div class="clear"></div>
                 <div id="colIzquierda">
                     <ul>
-                        <a href="#" class="btn btn-success"><li>Clients</li></a>
-                        <a href="#" class="btn btn-success"><li>Articles</li></a>
+                        <a href="#"  class="btn btn-success"><li>Clients</li></a>
+                        <a href="adminProductos.php" class="btn btn-success"><li>Products</li></a>
                         <a href="#" class="btn btn-success"><li>Categories</li></a>
                     </ul>
                 </div>
                 <div id="colDerecha">
                     <h4>Administer clients</h4>
-                    <div id="addSearch">
-                        <div id="search" class="input-group input-group-sm">
-                            <input type="text" class="form-control" placeholder="Search...">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default""><span class="glyphicon glyphicon-search"></span></button>
-                            </span>
-                        </div>
+					
+		<div id="addSearch">
+			<div id="search" class="input-group input-group-sm">
+			
+			<form id="searchForm">
+				<input type="text" class="form-control" name="nom" placeholder="Search...">
+				</form>
+				
+				<span class="input-group-btn" >
+					<a href="javascript:buscar()"><button action="buscar()" class="btn btn-default" ><span class="glyphicon glyphicon-search"></span></button></a>
+				</span>
+				
+			</div>
+						
+						
                         <div id="add">
                             <a class="btn btn-success btn-sm" data-toggle="modal" data-target="#myModal" href="#"><span class="glyphicon glyphicon-plus"></span> Add</a> 
                         </div>
                     </div>
-                    <div id="contieneTabla">
+					<div id="contieneTabla">
                         <table class="table" id="tabla">
                             <thead>
                                
@@ -183,8 +239,8 @@ function prepareUpdate(id){
      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
        <div class="modal-body">
 	  <div id="descrip">
-	  <div class="form-group" id="updateDiv" >
-						<form id="insertarCliente" class="form-horizontal" action="phps/insert.php" method="post">
+	  <div class="form-group"  >
+						<form id="insertarCliente" class="form-horizontal" >
 						Name<input class="form-control" name="nombre"></input>
 						Nick<input class="form-control" name="nick"></input>
 						E-mail<input class="form-control" name="email"></input>
@@ -209,61 +265,16 @@ function prepareUpdate(id){
     <div class="modal-content">
      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
        <div class="modal-body">
-	  <div id="descrip">
-	  <div class="form-group" id="registro" >
-						<form id="updatar" class="form-horizontal" action="phps/insert.php" method="post">
-						Name<input class="form-control" name="nombre"></input>
-						Nick<input class="form-control" name="nick"></input>
-						E-mail<input class="form-control" name="email"></input>
-						Password<input class="form-control"  type="password" name="pass"></input>
-						Repeat Password<input class="form-control" type="password" name="repPass"></input>
-						Client kind<select class="form-control"><option>Admin</option><option>User</option></select>
-								</div>
-						<a class="btn btn-success" href="javascript:insertar()">login</a>
-						
-						</form>
+	  <div id="updateDiv">
+	  <!--TODA LA DESCRIPCION DEL PRODUCTO-->
 						</div>
 
  </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+     
+     
         
-      </div>
+      
     </div>
   </div>
 </div>
 
-
-
-
-
-
-
-
-        <div id="pie">
-            <div id="LegalStuff">
-                <a href="">Legal Terms</a>
-                <a href="">Security</a>
-                <a href="">Privacy</a> 
-                <a href="">Rate</a> 
-                <a href="">Contact</a> 
-                <a href="">Web Map</a>
-            </div>
-
-
-            <div id="icons">
-                <img class="icon-footer" src="imagenes/icons/facebook24.png">
-                <img class="icon-footer" src="imagenes/icons/twitter14.png">
-                <img class="icon-footer" src="imagenes/icons/google17.png">
-                <img class="icon-footer" src="imagenes/icons/social68.png">
-            </div>
-            <div id="icons">
-                &copy; PC Store 2013. Spain. All rights reserved.<br />
-                Proyecto 2 - Tienda
-            </div>
-        </div>
-
-
-
-    </body>
-</html>
