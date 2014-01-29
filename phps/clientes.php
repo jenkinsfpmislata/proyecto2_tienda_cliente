f16b5d159ed21343ddf6af3499e1cd9778c87e2d
alumno <alumno@2A5PC22>
2014-01-29 11:37:16 +0100

    Todo sptint

--- phps/clientes.php
+++ phps/clientes.php
@@ -1,7 +1,7 @@
 <?php
 	
 	
-	$db=mysql_connect("localhost","root","")or die("Connection Error:");
+	$db=mysql_connect("localhost","root","frodo2013")or die("Connection Error:");
 	mysql_select_db("proyecto2_tienda")or die ("Error connecting db");
 	
 	$SQL="SELECT * FROM cliente;";
