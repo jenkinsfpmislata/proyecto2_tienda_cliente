7d5cd23292a2ed8c1d8367cc3c8072f0938fc403
alumno <alumno@2A5PC22>
2014-01-30 09:40:48 +0100

    Todo sptint

--- indexLogeado.php
+++ indexLogeado.php
@@ -256,7 +256,7 @@ $idCliente= $_GET["idCliente"];
 </div>
 	<?php
 	
-	$db=mysql_connect("localhost","root","")or die("Connection Error:");
+	$db=mysql_connect("localhost","root","frodo2013")or die("Connection Error:");
 	mysql_select_db("proyecto2_tienda")or die ("Error connecting db");
 	
 	$SQL="SELECT * FROM producto";
