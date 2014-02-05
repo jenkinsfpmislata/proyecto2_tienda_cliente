8c8e3cc81de0821d8a7387a8b6b7a2e8b06c20f4
JCVarea <alumno@2A5PC22>
2014-02-05 13:08:06 +0100

    tercer sprint, 0.5 mensajes a medias

--- index.php
+++ index.php
@@ -57,6 +57,8 @@
                     success: function(data) {
                         var datos = '<table>';
                         $.each(data, function(index) {
+                            index=0;
+                        
                             if(index<15){
                         
                             datos += '<div class="producto"><p id="precio">' + data[index].precio + ' &euro;</p><img id="imgProd" src="imagenes/imagenesProductos/' + data[index].Imagen + '.jpg"><div id="descripcion"><p>' + data[index].Nombre + '<br>' + data[index].Descripcion + '</p></div><a  ><div class="carrito" onclick="descripcion(' + data[index].idProducto + ')" data-toggle="modal" data-target="#myModalDescripcion"><img src="imagenes/imagenesStatic/carro.png"></div></a></div>';
